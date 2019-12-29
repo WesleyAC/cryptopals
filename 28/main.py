e840c02f11074f3aab7085ac07f6c9a7646182d1
Wesley Aptekar-Cassels <me@wesleyac.com>
2019-12-29 18:06:09 -0500

    Add #28

---
+++ 28/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from sha1 import Sha1Hash
+
+def gen_mac(key, msg):
+    return Sha1Hash().update(key + msg).digest()
+
+def check_mac(key, msg, mac):
+    return Sha1Hash().update(key + msg).digest() == mac
+
+if __name__ == "__main__":
+    msg = b"hello!"
+    key = b"foobar"
+    mac = gen_mac(key, msg)
+    print(mac)
+    assert check_mac(key, msg, mac)
+    assert not check_mac(key, b"something else", mac)
