cd9ef5272e193221ac950acd569831abb37ad950
Wesley Aptekar-Cassels <me@wesleyac.com>
2019-12-30 03:13:38 -0500

    Add #33

---
+++ 33/1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import random
+
+p = 37
+g = 35
+
+a = random.randint(0,p)
+A = (g ** a) % p
+b = random.randint(0,p)
+B = (g ** b) % p
+
+s1 = (B ** a) % p
+s2 = (A ** b) % p
+
+assert s1 == s2
